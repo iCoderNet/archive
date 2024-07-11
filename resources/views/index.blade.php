c11af60ee859e600e1ff76dde82d24f0c9b7bf3a
odiljonDev2007 <ramazanovodiljon@gmail.com>
2024-07-11 16:16:36 +0500

    lang

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,16 +18,8 @@
                 </div>
                 <div class="col-lg-6 order-lg-1 wow fadeInUp pt-5" data-wow-delay="0.3s">
                     <!-- <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">About Us</p> -->
-                    <h1 class="display-6 mb-4 text-primary">{{ __('tab1') }}</h1>
-                    <h5 style="font-weight: 600;" class="mb-4 text-muted">You are on the website of the Interactive Archive
-                        of the Germans of Kazakhstan, where are collected and presented the materials from state and family
-                        archives on the history and culture of the Kazakh Germans. When studying the past, we turn to
-                        various sources, including archival documents that silently store information about past events,
-                        phenomena, facts, and names. Is it really silent? By carefully collecting, researching and
-                        preserving information from archives on various topics in the history and culture of the Kazakh
-                        Germans, we saw that archival materials can often loudly “voice” certain problems of the past. And
-                        all the more valuable today is turning to documentary history in search of answers to many pressing
-                        questions and challenges of our time.</h5>
+                    <h1 class="display-6 mb-4 text-primary">{{ __('index.int') }}</h1>
+                    <h5 style="font-weight: 600;" class="mb-4 text-muted">{{ __('index.int') }}</h5>
                 </div>
             </div>
         </div>
@@ -43,15 +35,8 @@
                 </div>
                 <div class="col-lg-6 order-lg-1 wow fadeInUp" data-wow-delay="0.3s">
                     <!-- <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">About Us</p> -->
-                    <h1 class="display-6 mb-4 text-primary">WHAT IS THE ARCHIVE USEFUL FOR YOU PERSONALLY?</h1>
-                    <h5 style="font-weight: 600;" class="mb-4 text-muted">From 2022 to 2023 carried out the project team a
-                        lot of work in the state archives of the Republic of Kazakhstan, collecting materials from the
-                        family archives of Kazakh Germans. The identified set of materials was digitized and systematized
-                        into the presented categories. Using the site you will have the opportunity to get acquainted with
-                        various topics on the history and culture of the Germans of Kazakhstan. Please note that the site
-                        contains electronic copies of archival documents and the site does not provide archive certificates.
-                        To obtain archive certificates, you should contact departmental and state archival institutions of
-                        the Republic of Kazakhstan.</h5>
+                    <h1 class="display-6 mb-4 text-primary">{{ __('index.what') }}</h1>
+                    <h5 style="font-weight: 600;" class="mb-4 text-muted">{{ __('index.bigtitle2') }}</h5>
                 </div>
             </div>
         </div>
@@ -66,24 +51,16 @@
                 </div>
                 <div class="col-lg-6 order-lg-1 wow fadeInUp" data-wow-delay="0.3s">
                     <!-- <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">About Us</p> -->
-                    <h1 class="display-6 mb-4 text-primary">HOW IT WORK?</h1>
-                    <h5 style="font-weight: 600;" class="mb-4 text-muted">You can go to “Advanced Search” to see all the
-                        publications available on the site, divided into sections, or find the information you need
-                        specifically, and click on an area on the map on the right to see all publications related to this
-                        area. The same thing can be done using the “Search” menu item. By accessing the “Database” menu
-                        item, you can search for a specific person and information about him. If you have materials that you
-                        would like to submit for placement on our website, use the “Submit Materials” menu item.</h5>
-                    <p class="mb-4 text-muted">All materials on the site are provided for informational purposes only.
-                        Partial or complete copying of site materials without written permission from the site
-                        administration is prohibited! All materials on the site are protected in accordance with the law,
-                        including copyright and related rights.</p>
+                    <h1 class="display-6 mb-4 text-primary">{{ __('index.title2') }}</h1>
+                    <h5 style="font-weight: 600;" class="mb-4 text-muted">{{ __('index.bigtitle3') }}</h5>
+                    <p class="mb-4 text-muted">{{ __('index.bigtitle4') }}</p>
                     <div class="row">
                         <div class="col-12 col-md-6 mb-3 mb-md-0">
-                            <button class="btn btn-primary py-3 px-5 w-100" style="background-color: #0769B4;">Open the
-                                map</button>
+                            <button class="btn btn-primary py-3 px-5 w-100"
+                                style="background-color: #0769B4;">{{ __('index.map') }}</button>
                         </div>
                         <div class="col-12 col-md-6">
-                            <button class="btn btn-outline-primary py-3 px-5 w-100">Advanced search</button>
+                            <button class="btn btn-outline-primary py-3 px-5 w-100">{{ __('index.adsearch') }}</button>
                         </div>
                     </div>
                 </div>
--- resources/views/pages/person.blade.php
+++ resources/views/pages/person.blade.php
@@ -5,44 +5,50 @@
 @section('content')
     <div class="container">
         <div class="row mt-5">
-            <h2 class="text-primary mb-4">Lists of Germans forcibly evicted to the Kazakh SSR</h2>
-            <h4 class="text-muted">“Lists of Germans forcibly deported to the Kazakh SSR” is a database that contains information on the arrival and resettlement of Germans deported to Kazakhstan. More details</h4>
-            
-            <h2 class="text-primary my-4">Search</h2>
+            <h2 class="text-primary mb-4">{{ __('person.title') }}</h2>
+            <h4 class="text-muted">{{ __('person.bigtitle') }}</h4>
+
+            <h2 class="text-primary my-4">{{ __('person.search') }}</h2>
 
             <form class="contact-form" action="{{ route('person') }}" method="GET">
                 <div class="row mb-3">
                     <div class="col-12 col-sm-6">
-                        <label for="name" class="text-primary h5 font-weight-bold">Firstname</label>
-                        <input type="text" id="name" name="firstname" class="form-control search-inp mb-3" placeholder="Enter" value="{{ old('firstname', request('firstname')) }}">
+                        <label for="name" class="text-primary h5 font-weight-bold">{{ __('person.firstname') }}</label>
+                        <input type="text" id="name" name="firstname" class="form-control search-inp mb-3"
+                            placeholder="Enter" value="{{ old('firstname', request('firstname')) }}">
                     </div>
                     <div class="col">
-                        <label for="year-picker" class="text-primary h5 font-weight-bold">Year of birth</label>
-                        <input type="text" id="year-picker" name="birth_year" class="form-control search-inp mb-3" placeholder="Enter year" value="{{ old('birth_year', request('birth_year')) }}">
+                        <label for="year-picker" class="text-primary h5 font-weight-bold">{{ __('person.birth') }}</label>
+                        <input type="text" id="year-picker" name="birth_year" class="form-control search-inp mb-3"
+                            placeholder="Enter" value="{{ old('birth_year', request('birth_year')) }}">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-12 col-sm-6">
-                        <label for="lastname" class="text-primary h5 font-weight-bold">Lastname</label>
-                        <input type="text" id="lastname" name="lastname" class="form-control search-inp mb-3" placeholder="Enter" value="{{ old('lastname', request('lastname')) }}">
+                        <label for="lastname" class="text-primary h5 font-weight-bold">{{ __('person.lastname') }}</label>
+                        <input type="text" id="lastname" name="lastname" class="form-control search-inp mb-3"
+                            placeholder="Enter" value="{{ old('lastname', request('lastname')) }}">
                     </div>
                     <div class="col">
-                        <label for="object" class="text-primary h5 font-weight-bold">Object</label>
+                        <label for="object" class="text-primary h5 font-weight-bold">{{ __('person.object') }}</label>
                         <select name="object" id="object" class="form-control search-inp mb-3">
                             <option value="">Select</option>
                             <!-- Populate options here -->
                             <!-- Example: -->
-                            <option value="object1" {{ old('object', request('object')) == 'object1' ? 'selected' : '' }}>Object 1</option>
-                            <option value="object2" {{ old('object', request('object')) == 'object2' ? 'selected' : '' }}>Object 2</option>
+                            <option value="object1" {{ old('object', request('object')) == 'object1' ? 'selected' : '' }}>
+                                Object 1</option>
+                            <option value="object2" {{ old('object', request('object')) == 'object2' ? 'selected' : '' }}>
+                                Object 2</option>
                         </select>
                     </div>
                 </div>
                 <div class="row d-flex justify-content-center my-5">
                     <div class="col-12 col-md-6 col-lg-3 mb-3 mb-lg-0">
-                        <button type="submit" class="btn btn-primary btn-lg w-100 py-3">Search</button>
+                        <button type="submit" class="btn btn-primary btn-lg w-100 py-3">{{ __('person.search') }}</button>
                     </div>
                     <div class="col-12 col-md-6 col-lg-3">
-                        <button type="reset" class="btn btn-outline-primary btn-lg w-100 py-3" id="clear">Clear</button>
+                        <button type="reset" class="btn btn-outline-primary btn-lg w-100 py-3"
+                            id="clear">{{ __('person.clear') }}</button>
                     </div>
                 </div>
                 @if ($persons->currentPage() > 1 || $persons->perPage() != 10)
@@ -55,12 +61,12 @@
                 <table class="table table-striped person-table mt-5">
                     <thead>
                         <tr>
-                            <th scope="col">Lastname</th>
-                            <th scope="col">Firstname</th>
-                            <th scope="col">Surname</th>
-                            <th scope="col">Year</th>
-                            <th scope="col">Birth Place</th>
-                            <th scope="col">Object</th>
+                            <th scope="col">{{ __('person.lastname') }}</th>
+                            <th scope="col">{{ __('person.firstname') }}</th>
+                            <th scope="col">{{ __('person.surname') }}</th>
+                            <th scope="col">{{ __('person.year') }}</th>
+                            <th scope="col">{{ __('person.birthplace') }}</th>
+                            <th scope="col">{{ __('person.object') }}</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -75,7 +81,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="6" class="text-center">No Data</td>
+                                <td colspan="6" class="text-center">{{ __('person.nodata') }}</td>
                             </tr>
                         @endforelse
                     </tbody>
@@ -120,7 +126,7 @@
             $(".ui-datepicker-calendar").hide();
         }).on('input', function() {
             let vvl = $(this).val();
-            if (vvl.length == 4){
+            if (vvl.length == 4) {
                 $(this).datepicker('setDate', new Date(vvl, 1));
                 $(".ui-datepicker-month").hide();
                 $(".ui-datepicker-calendar").hide();
