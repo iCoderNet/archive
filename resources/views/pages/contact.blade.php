6eb647255f6790862ffc842c556f3ba9eb48dbfb
odiljonDev2007 <ramazanovodiljon@gmail.com>
2024-07-11 13:42:03 +0500

    qw

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -5,25 +5,30 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2 class="text-primary mb-4">Contacts</h2>
+            <h2 class="text-primary mb-4">{{ __('contact.title') }}</h2>
             <div class="col-md-6 order-lg-2 order-md-2">
                 <div class="illustration d-flex flex-column justify-content-center" style="position: relative;">
                     <img src="/assets/img/contact/contacts.f4cd78bd.svg" class="img-fluid" alt="Illustration">
                     <div class="suggest-sidebar mb-5">
-                        <h6 class="text-primary">You can contact us in a way that is convenient for you</h6>
+                        <h6 class="text-primary">{{ __('contact.imgtext') }}</h6>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="d-flex align-items-center justify-content-between gap-4 flex-wrap flex-sm-nowrap">
-                    <a href="mailto:de.archiv.kz2022@gmail.com" class="contact-method w-100 w-sm-100 p-4 d-flex flex-column justify-content-center" style="position: relative;">
-                        <i class="bi bi-envelope-fill text-primary" style="font-size: 30px; position: absolute; top: 20%; right: 10%;"></i>
+                    <a href="mailto:de.archiv.kz2022@gmail.com"
+                        class="contact-method w-100 w-sm-100 p-4 d-flex flex-column justify-content-center"
+                        style="position: relative;">
+                        <i class="bi bi-envelope-fill text-primary"
+                            style="font-size: 30px; position: absolute; top: 20%; right: 10%;"></i>
                         <span class="text-primary">Write</span>
                         <h4>to email</h4>
                         <span class="text-primary">de.archiv.kz2022@gmail.com</span>
                     </a>
-                    <a href="tel:77172429395" class="contact-method w-100 p-4 d-flex flex-column justify-content-center" style="position: relative;">
-                        <i class="bi bi-telephone text-primary" style="font-size: 30px; position: absolute; top: 20%; right: 10%;"></i>
+                    <a href="tel:77172429395" class="contact-method w-100 p-4 d-flex flex-column justify-content-center"
+                        style="position: relative;">
+                        <i class="bi bi-telephone text-primary"
+                            style="font-size: 30px; position: absolute; top: 20%; right: 10%;"></i>
                         <span class="text-primary">Call</span>
                         <h4>by number</h4>
                         <span class="text-primary">+7 7172 429395</span>
@@ -31,30 +36,33 @@
                 </div>
                 <div class="form-container mt-4">
                     <h4 class="mb-3">Contact us by filling out the form below</h4>
-                    @if(session('success'))
+                    @if (session('success'))
                         <div class="alert alert-success" role="alert">{{ session('success') }}</div>
                     @endif
                     <form class="contact-form" action="{{ route('contact.submit') }}" method="POST">
-                        @csrf  
+                        @csrf
                         <div class="form-group">
                             <i class="bi bi-person contact-form-icon"></i>
-                            <input type="text" class="form-control mb-3 ps-5" id="name" name="name" placeholder="Last and First name" required>
+                            <input type="text" class="form-control mb-3 ps-5" id="name" name="name"
+                                placeholder="Last and First name" required>
                         </div>
                         <div class="form-group">
                             <i class="bi bi-envelope contact-form-icon"></i>
-                            <input type="email" class="form-control mb-3 ps-5" id="email" name="email" placeholder="E-mail" required>
+                            <input type="email" class="form-control mb-3 ps-5" id="email" name="email"
+                                placeholder="E-mail" required>
                         </div>
                         <div class="form-group">
                             <i class="bi bi-phone contact-form-icon"></i>
-                            <input type="tel" class="form-control mb-3 ps-5" id="phone" name="phone" placeholder="Phone number" required>
+                            <input type="tel" class="form-control mb-3 ps-5" id="phone" name="phone"
+                                placeholder="Phone number" required>
                         </div>
                         <div class="form-group">
                             <textarea class="form-control mb-3" id="message" name="message" placeholder="Write a message" rows="8" required></textarea>
                         </div>
-                        @if($errors->any())
+                        @if ($errors->any())
                             <div>
                                 <ul>
-                                    @foreach($errors->all() as $error)
+                                    @foreach ($errors->all() as $error)
                                         <li style="color: red">{{ $error }}</li>
                                     @endforeach
                                 </ul>
@@ -64,7 +72,7 @@
                     </form>
                 </div>
             </div>
-            
+
         </div>
     </div>
 @endsection
